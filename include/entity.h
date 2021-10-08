f66ff50c6b52f29bd348b8d266aa838388602259
MusaTamzid05 <tamzid101@gmail.com>
2021-10-08 08:43:35 +0600

    renamed entity.

--- include/entity.h
+++ include/entity.h
@@ -1,22 +1,27 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <string>
+
 namespace Graphics {
     class Shader;
 
     class Mesh {
         public:
-            Mesh() {}
+            Mesh(const std::string& texture_path = "") {}
             virtual void draw() = 0;
+
+            virtual bool load_texture(const std::string& texture_path);
 
             unsigned int VAO;
             unsigned int VBO;
+            unsigned int texture_id;
 
     };
 
-    class Enity {
+    class Entity {
         public:
-            Enity(Shader* shader, Mesh* mesh);;
+            Entity(Shader* shader, Mesh* mesh);;
 
             virtual void draw() = 0;
 
