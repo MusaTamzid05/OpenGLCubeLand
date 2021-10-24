a37cbc61995edfd11d832c00af18fee98370eaa7
MusaTamzid05 <tamzid101@gmail.com>
2021-10-24 19:14:48 +0600

    map initialize with mesh type.

--- include/mesh_factory.h
+++ include/mesh_factory.h
@@ -1,15 +1,30 @@
 #ifndef MESH_FACTORY_H
 #define MESH_FACTORY_H
 
+
+#include <map>
+
+namespace Graphics {
+    class Mesh;
+};
+
 namespace Factory {
 
     class MeshFactory {
         public:
+
+            enum Type {
+                Cube
+            };
+
             static MeshFactory* GetInstance();
+            Graphics::Mesh* get_mesh(const Type& type) ;
 
         private:
-            MeshFactory() {}
+            MeshFactory();
             static MeshFactory* m_instance;
+
+            std::map<Type, Graphics::Mesh*> mesh_list;
     };
 }
 
--- src/mesh_factory.cpp
+++ src/mesh_factory.cpp
@@ -1,12 +1,24 @@
 #include "mesh_factory.h"
+#include "entity.h"
+#include "cube.h"
 
 namespace Factory {
     MeshFactory* MeshFactory::m_instance = nullptr;
+
+    MeshFactory::MeshFactory() {
+        mesh_list.insert(std::pair<Type, Graphics::Mesh*>(Type::Cube, new Cube::Mesh("../textures/container.jpg")));
+
+    }
 
     MeshFactory* MeshFactory::GetInstance() {
         if(m_instance == nullptr)
             m_instance = new MeshFactory();
 
         return m_instance;
+    }
+
+
+    Graphics::Mesh* MeshFactory::get_mesh(const Type& type) {
+        return mesh_list[type];
     }
 }
